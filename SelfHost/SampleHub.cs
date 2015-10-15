8c3e8a10874c4f9464fc77f6be82224406b8aeaf
rinoreji <mailrino@gmail.com>
2015-10-16 00:11:39 +0530

    initial commit

---
+++ SelfHost/SampleHub.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelfHost
+{
+    public class SampleHub : Hub
+    {
+        public void Send(string name, string message)
+        {
+            Clients.All.addMessage(name, message);
+        }
+
+        public string SampleMethod(string input)
+        {
+            return string.Format("{0} - Message received: '{1}'", DateTime.Now.ToString(), input);
+        }
+    }
+}
